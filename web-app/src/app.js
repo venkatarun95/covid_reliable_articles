ae6ec32a7a046917c298f583fe0229f1d89b1369
Venkat Arun <venkatarun95@gmail.com>
2021-07-23 13:01:19 -0700

    Improved the UI

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -27,6 +27,51 @@ const search = instantsearch({
   indexName: "reliable_articles"
 });
 
+function url_to_source(url) {
+    const parsed = new URL(url);
+    const sources = [
+    	["thelancet", "The Lancet"],
+    	["nejm", "New England Journal of medicine"],
+    	["jamanetwork", "JAMA"],
+    	["scientificamerican", "Scientific American"],
+    	["cdc.gov", "CDC USA"],
+    	["nature", "Nature"],
+    	["sciencemag", "Science"],
+    	["pfizer", "Pfizer"],
+    	["modernatx", "Moderna"],
+    	["yourlocalepidemiologist", "Your Local Epidemiologist"],
+    	["jnj", "JnJ"],
+    	["astrazeneca", "Astra Zeneca"],
+    	["bmj", "BMJ"],
+    	["echnologyreview", "Technology Review"],
+    	["globalhealth.stanford", "Stanford Univ."],
+    	["iisc", "Indian Institute of Science"],
+    	["caltech", "CalTech"],
+    	["hopkinsmedicine", "Hopkins Medicine"],
+    	["ucsf", "UC San Francisco"],
+    	["unbiasedscipod", "Unbiased Science Podcast"],
+    	["hoodmedicine", "Hood Medicine"],
+    	["princeton", "Princeton Univ."],
+    	["theconversation", "The Conversation"],
+    	["dearpandemic", "Dear Pandemic"],
+    	["firstdraft", "First Draft News"],
+    	["who.int", "WHO"],
+    	["nih.gov", "NIH USA"],
+    	["nationalgeographic", "National Geographic"]
+    ];
+    for (let i in sources) {
+    	if (parsed.host.indexOf(sources[i][0]) != -1) {
+    	    return sources[i][1];
+    	}
+    }
+    return parsed.host;
+}
+
+function ts_to_date(ts) {
+    const date = new Date(ts * 1000);
+    return date.toDateString();
+}
+
 search.addWidgets([
     instantsearch.widgets.searchBox({
 	container: '#searchbox',
@@ -34,18 +79,58 @@ search.addWidgets([
     instantsearch.widgets.hits({
 	container: '#hits',
 	templates: {
-            item: `
-          <div>
-            <img src="" align="left" alt="" />
-            <div class="hit-name">
-              {{#helpers.highlight}}{ "attribute": "google_title" }{{/helpers.highlight}}
-            </div>
-            <div class="hit-description">
-              {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
-            </div>
-            <img height=75 width=225 src="{{#helpers.highlight}}{ "attribute": "top_image" }{{/helpers.highlight}}" />
-          </div>
-        `,
+	    item(hit) {
+		// See how long the snippet is
+		const snippet = instantsearch.snippet({ attribute: 'text', hit });
+		let display_text;
+		if (snippet.length > 200) {
+		    // Sometimes description has "2 days ago...", remove it
+		    const remove = hit.description.indexOf("ago ...");
+		    if (remove != -1) {
+			display_text = hit.description.substring(remove + 7);
+		    }
+		    else {
+			display_text = hit.description
+		    }
+		}
+		else {
+		    display_text = snippet
+		}
+
+		return `
+<a href=${hit.url}><b>
+  <div><b>
+      ${instantsearch.snippet({ attribute: 'title', hit })}
+    </b></div>
+<img src="${hit.top_image}" alt=" " class="img-fluid img-thumbnail"/>
+</a>
+<div>${display_text}</div>
+<div>
+  <span class="badge rounded-pill bg-light text-dark">${url_to_source(hit.url)}</span>
+  <span class="badge rounded-pill bg-light text-dark" style="float: right">${ts_to_date(hit.date)}</span>
+</div>
+
+                `;
+	    }
+        //     item: `
+        //     <span><script>document.write("Hello")</script></span>
+        //   <div>
+        //     <img src="" align="left" alt="" />
+        //     <div class="hit-name"><b>
+        //       <a href={{ url }}>
+        //         {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+        //       </a>
+        //     </b></div>
+        //     <img height=75 width=225 src="{{#helpers.highlight}}{ "attribute": "top_image" }{{/helpers.highlight}}" />
+        //     <div class="hit-description">
+        //       {{#helpers.snippet}}{ "attribute": "text" }{{/helpers.snippet}}
+        //     </div>
+
+        //   <!--<div id="publisher{{id}}"></div>
+        //   <script>$('#publisher{{id}}').text("Hello")</script> -->
+
+        //   </div>
+        // `,
 	},
     }),
     instantsearch.widgets.pagination({
